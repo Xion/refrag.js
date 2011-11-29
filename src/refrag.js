f05c034bd6016c9a39ffefb8aa9ca0da9c1a03fc
Karol Kuczmarski <xion.dev@gmail.com>
2011-11-29 14:50:51 +0100

    Almost finished (but not tested, so...)

--- src/refrag.js
+++ src/refrag.js
@@ -77,87 +77,118 @@
 
     /** User interaction **/
 
-    var highlightText = function(text, $elem) {
-        var elemsToHighlight = [];
+    var highlightText = (function() {
 
-        // for every text node in matched element(s),
-        // we inject <span>'s that surround the matched text itself
-        // so that we can highlight it
-        var textNodes = $elem.isText() ? [$elem] : findMatchingTextNodes($elem, text);
-        for (var idx in textNodes) {
-            var $highlightElem = injectElementAroundText(textNodes[idx]);
-            elemsToHighlight.push($highlightElem);
-        }
+        var findTextNodes = function($node) {
+            /** Retrieves all text nodes contained within given DOM node.
+                Returns a list of such nodes in depth-first order
+                (that is, the order they should appear on page, assuming position:static). */
+            var textNodes = [];
 
-        for (var idx in elemsToHighlight)
-            elemsToHighlight[idx].css('background-color', 'yellow');
-    };
-
-    var injectElementAroundText = function($node, text) {
-        /** Injects a <span> element inside given text node. The element
-            will surround given text.  */
-        var nodeText = $node.text();
-        var start = nodeText.indexOf(text);
-        var end = start + text.length;
-
-        var $textBefore = $(document.createTextNode(nodeText.substring(0, start)));
-        var $textAfter = $(document.createTextNode(nodeText.substring(end)));
-        var $matchedText = $('<span/>').html(text);
-
-        var $elemWrapper = $('<span/>');
-        $elemWrapper.append($textBefore).append($matchedText).append($textAfter);
-        $node.replaceWith($elemWrapper);
-
-        return $matchedText;
-    };
-
-    var findMatchingTextNodes = function($elem, text) {
-        /** Retrieves the text nodes (within given DOM node)
-            that contain given text.
-            Note that first and last element of resulting list
-            might contain some extra text as prefix and suffix, respectively. */
-        var elemTextNodes = findTextNodes($elem);
-        
-        var nodeIdx = 0;
-        while (nodeIdx < elemTextNodes.length) {
-            // build a potential match
-            var maybeMatch = "";
-            var i = nodeIdx;
-            while (i < elemTextNodes.length && maybeMatch.length < text.length) {
-                maybeMatch += elemTextNodes[i];
-                i++;
+            var elemStack = [];
+            elemStack.push($node);
+            while (elemStack.length > 0) {
+                var $this = $(elemStack.pop());
+                if ($this.isText()) textNodes.push($this);
+                else {
+                    var children = $this.contents();
+                    for (var idx in children)
+                        elemStack.push(children[idx]);
+                }
             }
 
-            if (maybeMatch.indexOf(text) >= 0)
-                return elemTextNodes.slice(nodeIdx, i);    
+            return textNodes;
+        };
+
+        var findMatchingTextNodes = function($elem, text) {
+            /** Retrieves the text nodes (within given DOM node)
+                that contain given text.
+                Note that first and last element of resulting list
+                might contain some extra text as prefix and suffix, respectively.
+                @return A list of objects with following properties:
+                        - node - A text node
+                        - offset - Offset inside text node where the matching text starts
+                                   (>0 only for first item)
+                        - length - Length of portion of matched text that lies inside the node
+                                   (<node.text().length only for last item) */
+            var elemTextNodes = findTextNodes($elem);
             
-            // no match - continue from the last text node we considered
-            nodeIdx = i;
-        }
+            var nodeIdx = 0;
+            while (nodeIdx < elemTextNodes.length) {
+                // build a potential match
+                var maybeMatch = "";
+                var i = nodeIdx;
+                while (i < elemTextNodes.length && maybeMatch.length < text.length) {
+                    maybeMatch += elemTextNodes[i];
+                    i++;
+                }
 
-        return [];
-    };
+                var matchIndex = maybeMatch.indexOf(text);
+                if (matchIndex >= 0) {
+                    // match found, construct result
+                    var result = [];
+                    var posInText = 0;
+                    for (var k = nodeIdx; k < i; ++k) {
+                        var textNode = elemTextNodes[k];
 
-    var findTextNodes = function($node) {
-        /** Retrieves all text nodes contained within given DOM node.
-            Returns a list of such nodes in depth-first order
-            (that is, the order they should usually appear on page). */
-        var textNodes = [];
+                        var item = {node: textNode};
+                        item.offset = k == nodeIdx ? matchIndex : 0;
 
-        var elemStack = [];
-        elemStack.push($node);
-        while (elemStack.length > 0) {
-            var $this = $(elemStack.pop());
-            if ($this.isText()) textNodes.push($this);
-            else {
-                var children = $this.contents();
-                for (var idx in children)
-                    elemStack.push(children[idx]);
+                        var d = item.offset > 0 ? item.offset + 1 : posInText;
+                        item.length = Math.min(nodeTextLength, textNode.text().length - d);
+                        
+                        posInText += item.length;
+                    }
+                }
+                
+                // no match - continue from the last text node we considered
+                nodeIdx = i;
             }
-        }
 
-        return textNodes;
-    };
+            return [];
+        };
+
+        var injectElementAroundText = function($node, text, elemName) {
+            /** Injects an element inside given text node.
+                The new element surrounds given text, while the rest
+                of the text inside node will form new text nodes.  */
+            elemName = elemName || 'span';
+            
+            var nodeText = $node.text();
+            var start = nodeText.indexOf(text);
+            var end = start + text.length;
+
+            var $textBefore = $(document.createTextNode(nodeText.substring(0, start)));
+            var $textAfter = $(document.createTextNode(nodeText.substring(end)));
+            var $matchedText = $('<' + elemName + '/>').html(text);
+
+            var $elemWrapper = $('<' + elemName + '/>');
+            $elemWrapper.append($textBefore).append($matchedText).append($textAfter);
+            $node.replaceWith($elemWrapper);
+
+            return $matchedText;
+        };
+    
+        /** Actual highlighting function. */
+        return function(text, $elem) {
+            var elemsToHighlight = [];
+
+            // for every text node in matched element(s),
+            // we inject <span>'s that surround the matched text itself
+            // so that we can highlight it
+            var textNodes = $elem.isText() ? [$elem] : findMatchingTextNodes($elem, text);
+
+
+            
+            for (var idx in textNodes) {
+                var $highlightElem = injectElementAroundText(textNodes[idx], text);
+                elemsToHighlight.push($highlightElem);
+            }
+
+            for (var idx in elemsToHighlight)
+                elemsToHighlight[idx].css('background-color', 'yellow');
+        };
+    })();
 
 })(
 
